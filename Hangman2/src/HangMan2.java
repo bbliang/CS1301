0d5c4d3e728731b7d2773f224e0f3dec4164b2aa
Wylis Stark <bryanliang7@gmail.com>
2017-07-28 20:26:13 -0400

    Update HangMan2.java

--- Hangman2/src/HangMan2.java
+++ Hangman2/src/HangMan2.java
@@ -1,26 +1,3 @@
-/*
-* Hangman2.java
-* Author: Bryan Liang
-* Submission Date: March 4, 2017
-*
-* Purpose: Succ me so gud im lik got damn u dirty bich
-*
-* Statement of Academic Honesty:
-*
-* The following code represents my own work. I have neither
-* received nor given inappropriate assistance. I have not copied
-* or modified code from any source other than the course webpage
-* or the course textbook. I recognize that any unauthorized
-* assistance or plagiarism will be handled in accordance with
-* the University of Georgia's Academic Honesty Policy and the
-* policies of this course. I recognize that my work is based
-* on an assignment created by the Department of Computer
-* Science at the University of Georgia. Any publishing
-* or posting of source code for this project is strictly
-* prohibited unless you have written consent from the Department
-* of Computer Science at the University of Georgia.
-*/
-
 import java.util.Scanner;
 
 public class HangMan2 {
